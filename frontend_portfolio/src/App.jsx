e8b46d00801d7300dd8593b2f1eff76bbf12261e
Aduloju James <adulojujames@gmail.com>
2022-10-20 13:05:10 +0100

    container completed

---
+++ frontend_portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {About, Footer, Header, Skills, Testimonial, Work} from './container';
+
+const App = () => {
+  return (
+   <div className='app'>
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+   </div>
+  )
+}
+
+export default App
